07629f0f612af7b1838d9a84e0dbadf6b388dc4f
Adar Nimrod <nimrod@shore.co.il>
2017-04-16 10:12:52 +0300

    - Correct test, use users_sudo_group instead of sudoers.

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,8 +3,9 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_users(Command, Ansible):
-    assert Command('''ssh dummy@localhost sudo whoami''').stdout == 'root'
+def test_users(Command, Ansible, Sudo):
+    with Sudo():
+        assert Command('''ssh dummy@localhost sudo whoami''').stdout == 'root'
 
 
 def test_users_prune(Command):
